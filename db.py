7cbcf0021c94b14f3164f3e8b8c578ef0f87fb75
LSD <luciferiidem@gmail.com>
2024-07-23 11:54:19 +0700

    Fix version 1

--- db.py
+++ db.py
@@ -44,15 +44,29 @@ def add_phone(conn, client_id, phones):
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     with conn.cursor() as cur:
-        cur.execute("""
-                    update client set first_name = %s, last_name = %s, email = %s where id = %s
-                    """, (first_name, last_name, email, client_id)
-                    )
+        if first_name != "":
+            cur.execute("""
+                        update client set first_name = %s where id = %s
+                        """, (first_name,  client_id)
+                        )
 
-        cur.execute("""
-                    update client_phones set phone = %s where client_id = %s
-                    
-                    """, (phones, client_id))
+        if last_name != "":
+            cur.execute("""
+                        update client set last_name = %s where id = %s
+                        """, (last_name, client_id)
+                        )
+
+        if email != "":
+            cur.execute("""
+                        update client set email = %s where id = %s
+                        """, (email, client_id)
+                        )
+
+        if phones != "":
+            cur.execute("""
+                        update client_phones set phone = %s where client_id = %s
+                        """, (phones, client_id)
+                        )
 
 
 def delete_client(conn, client_id):
@@ -74,21 +88,22 @@ def delete_phone(conn, client_id, phone):
                     """, (client_id, phone))
 
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+def find_client(conn, first_name="%", last_name="%", email="%", phone="%"):
     with conn.cursor() as cur:
         cur.execute("""
                     select first_name,last_name,email,phone from client LEFT JOIN client_phones  ON client.id = client_phones.client_id
-where first_name =%s or last_name = %s  or email like %s or phone like %s ;
-                    """, (first_name, last_name, email, phone))
+where lower(first_name) like %s and lower(last_name) like %s  and lower(email) like %s  and phone like %s ;
+                    """, (f'%{first_name.lower()}%', f'%{last_name.lower()}%', f'%{email.lower()}%', f'%{phone}%'))
         print(cur.fetchall())
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="Ведите свой пароль от Postgres") as conn:
+if __name__ == '__main__':
+    with psycopg2.connect(database="clients_db", user="postgres", password="Введите пароль от базы") as conn:
 
-    # create_db(conn)
-    # add_client(conn, "Петр", "Петров", "J9zQz@example.com", "+7-888-999-99-99")
-    # add_phone(conn,"2", "+7-778-999-00-00")
-    # change_client(conn, "1", "Антон", "Путин", "ylTgI@example.com", "+7-905-999-99-99")
-    # delete_client(conn, "3")
-    # delete_phone(conn, "4", "+7-888-999-99-99")
-    find_client(conn, "Петр", "", "", "")
+        # create_db(conn)
+        # add_client(conn, "Петр", "Петров", "J9zQz@example.com", "+7-888-999-99-99")
+        # add_phone(conn,"4", "+7-778-999-00-00")
+        # change_client(conn, "2", "Иван", "Грозный", "", "+7-555-999-99-99")
+        # delete_client(conn, "3")
+        # delete_phone(conn, "4", "+7-888-999-99-99")
+        find_client(conn, "ВА")
